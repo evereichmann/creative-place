2c658220933e047feea7da08a8b4d27d6a1e6008
Eve Reichmann <eve.reichmann@gmail.com>
2020-09-16 22:43:22 -0500

    dino on mainpage

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,6 +7,7 @@ const Main = () => {
                <div id="main-subcontent">
                     <img id="rock" height="150" src="https://i.ibb.co/TBPc1tZ/Screen-Shot-2020-09-16-at-9-27-10-PM.png"/>
                     <h1 id="rock-text">Hello World</h1>
+                    <img id="dino" src="https://i.ibb.co/xXMjDBj/Screen-Shot-2020-09-16-at-9-45-40-PM.png"/>
                 </div> 
             </div>
         </div>
